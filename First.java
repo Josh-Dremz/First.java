08c616a9e3dec8841ccb582d3428bd2f76b1607b
Josh-Dremz <joshuachukwuneso@gmail.com>
2024-09-17 13:03:47 -0700

    test 2

---
+++ First.java
@@ -0,0 +1,26 @@
+public class First { 
+
+    public static void main(String[] args) {
+
+        System.out.println("Gifted hands");
+        System.out.println("Github test 2");
+
+
+    
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
