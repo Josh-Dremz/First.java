98af1d665626bd6cd9f1163eb1a4490464c9dbc8
Josh-Dremz <joshuachukwuneso@gmail.com>
2024-09-17 04:32:19 -0700

    First commit

---
+++ assignment.java
@@ -0,0 +1,25 @@
+public class assignment {
+    public static void main(string[] args) { //MAIN METHOD 
+    System.out.println( "Brief about me");    
+
+    int name = 3 
+    String current = "currently learning video and photo editing";
+    String plans = "i aspire to become a content creator";
+    String hoddies = "video editing, creating and gaming";
+    String belief = "Christianity";
+
+    System.out.println(name);
+    String.out.println(current);
+    String.out.println(plans);
+    String.out.println(hoddies);
+
+
+
+
+
+
+
+
+}
+    
+}
